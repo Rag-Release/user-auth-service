727c1c63d9c082a64915606097cb941049e43594
ifham-IM <ifhammohamed1111@gmail.com>
2024-12-03 12:17:22 +0530

    refactor user model and migration to add additional address and identification fields

---
+++ src/data-access/sequelize/migrations/20241203063915-add-user-columns-homeaddress-delivery-address-phonenumber.js
@@ -0,0 +1,49 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn("Users", "homeAddress", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+    await queryInterface.addColumn("Users", "deliveryAddress", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+    await queryInterface.addColumn("Users", "phoneNumbers", {
+      type: Sequelize.STRING, // Store as JSON to handle an array
+      allowNull: true,
+    });
+    await queryInterface.addColumn("Users", "pickupPoint", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+    await queryInterface.addColumn("Users", "company", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+    await queryInterface.addColumn("Users", "fiscalCode", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+    await queryInterface.addColumn("Users", "cardNumber", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+    await queryInterface.addColumn("Users", "cardExpiry", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn("Users", "homeAddress");
+    await queryInterface.removeColumn("Users", "deliveryAddress");
+    await queryInterface.removeColumn("Users", "phoneNumbers");
+    await queryInterface.removeColumn("Users", "pickupPoint");
+    await queryInterface.removeColumn("Users", "company");
+    await queryInterface.removeColumn("Users", "fiscalCode");
+    await queryInterface.removeColumn("Users", "cardNumber");
+    await queryInterface.removeColumn("Users", "cardExpiry");
+  },
+};
